405e2355cf1ebacefa114b071be9e233455b1dd9
nawiid-ml <navidmala495@gmail.com>
2023-12-14 12:58:34 +0330

    Update :

--- flights/admin.py
+++ flights/admin.py
@@ -2,9 +2,12 @@ from django.contrib.admin import ModelAdmin, register
 from .models import flight,Airport
 @register(flight)
 class flight_admin(ModelAdmin):
-    pass
+    autocomplete_fields=['origin']
+
 @register(Airport)
 class Airport_admin(ModelAdmin):
-    pass
+    list_display=("Name","No")
+    search_fields=["Name"]
+    list_filter=["Name","City"]
 
 
--- flights/models.py
+++ flights/models.py
@@ -10,12 +10,21 @@ class Airport (models.Model):
     open_time=models.CharField(max_length=100)
     close_time=models.CharField(max_length=100)
 
+    class Meta:
+        verbose_name="Kelassor Airport"
+
 
 class flight(models.Model):
     origin=models.ForeignKey(Airport,on_delete=models.CASCADE)
     name=models.CharField(max_length=250)
     flight_number=models.IntegerField()
     seat=models.IntegerField()
-    price=models.IntegerField()
+    price=models.IntegerField(help_text="price is Rial")
+
+    def __str__(self) -> str:
+        return "{}-{}".format (self.name,self.seat)
+    
+    class Meta:
+        verbose_name="Kelassor Flight"
 
 
