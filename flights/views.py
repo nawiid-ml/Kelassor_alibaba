0c1c9b71255901a545eb58612f4079171c359318
nawiid-ml <navidmala495@gmail.com>
2023-12-14 14:59:02 +0330

    update: django patern views list /and list2

--- flights/views.py
+++ flights/views.py
@@ -1,9 +1,30 @@
 from django.http.response import HttpResponse , JsonResponse
+from .models import flight,Airport
 def list(request):
-    flight_dic={
-        "flight number ":458,
-        "name":"first class",
-        "seat ":2565,
-        "price":5698568
+    flights=flight.objects.all()
+    flight_list=[]
+    for item in flights:
+        dec={
+            "origin":item.origin.Name,
+            "flight_number":item.flight_number
+
         }
-    return JsonResponse(flight_dic)
+        flight_list.append(dec)
+    return HttpResponse(flight_list)
+
+def list2(request):
+    air=Airport.objects.all()
+    air_list=[]
+    for item in air:
+        dic_air={
+            "Name": item.Name,
+            "No": item.No,
+            "City":item.City,
+            "Address":item.Address,
+            "Phone_Number":item.Phone_Number,
+            "open_time":item.open_time,
+            "close_time":item.close_time
+        }
+        air_list.append(dic_air)
+    return HttpResponse(air_list)
+
